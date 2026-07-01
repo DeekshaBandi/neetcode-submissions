786157fb1e742fc5e05107f71f6dd8d5106ef248
DeekshaBandi <Bandi.Deeksha.24@gmail.com>
2026-07-01 17:10:30 -0500

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        
+        output = ""
+        for s in strs:
+            output += str(len(s)) + '.' + s
+        return output 
+
+
+
+
+    def decode(self, s: str) -> List[str]:
+        if not s:
+            return []
+        
+        output = []
+        i = 0
+
+        while i < len(s):
+            j = i
+            while s[j] != ".":
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length 
+            output.append(s[i:j])
+            i = j
+        return output 
